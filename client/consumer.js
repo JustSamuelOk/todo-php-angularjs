ffaa28e6c2bdbdafa68792c396a80ed06039a5e3
JustSamuelOk <samuelrasmussendrr@gmail.com>
2023-11-08 18:02:36 +0100

    Formatting

--- client/consumer.js
+++ client/consumer.js
@@ -1,32 +1,32 @@
 var app = angular.module('todo-app', []);
 
-app.controller('todoController', function($scope, $http) {
+app.controller('todoController', function ($scope, $http) {
     let apiUrl = 'http://localhost:8000/todos';
     $scope.newTodoTitle = "";
     $scope.newTodoDescription = "";
     $scope.todos = [];
 
-    $scope.getTodos = function() {
-        $http.get(apiUrl).then(function(response) {
+    $scope.getTodos = function () {
+        $http.get(apiUrl).then(function (response) {
             $scope.todos = response.data;
         });
     }
 
-    $scope.addTodo = function() {
+    $scope.addTodo = function () {
         $scope.newTodo = {
             title: $scope.newTodoTitle,
             description: $scope.newTodoDescription
         };
-        $http.post(apiUrl, $scope.newTodo).then(function() {
+        $http.post(apiUrl, $scope.newTodo).then(function () {
             $scope.newTodoTitle = "";
             $scope.newTodoDescription = "";
             $scope.getTodos();
         });
     }
 
-    $scope.deleteTodo = function(id) {
-        $http.post(apiUrl + '/' + id + '/delete').then(function() {
-            
+    $scope.deleteTodo = function (id) {
+        $http.post(apiUrl + '/' + id + '/delete').then(function () {
+
             $scope.getTodos();
         });
     }
